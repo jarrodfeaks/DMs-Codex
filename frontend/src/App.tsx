9aa129f531a4b5709d71483ba10201ffabcfedb5
MosaabSaleem <mmosaab2002@hotmail.com>
2024-09-05 20:37:21 +1000

    fixed up the colours on the example screen to match the theme colours

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 // App.tsx
 import React from 'react';
+import { createTheme } from '@mui/material';
 import LoginPage from './LoginPage';
+
+const theme = createTheme({
+  palette: {}
+});
 
 const App: React.FC = () => {
   return (
--- frontend/src/LoginPage.tsx
+++ frontend/src/LoginPage.tsx
@@ -4,20 +4,34 @@ import {TextField, Box, Button, Container, Avatar, Typography} from "@mui/materi
 const LoginPage: FC = () => {
 
   return (
-    <Container>
-        <Box sx={{marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-          <Avatar sx={{bgcolor: 'red'}}></Avatar>
-          <Typography variant='h5'>
-            Sign In
-          </Typography>
-          <TextField id='email' label='Email Address' autoComplete='email' autoFocus/>
-            
-          <TextField id='password' label='Password' type='password'/>
-          <Button variant='contained'>
-            Sign In
-          </Button>
-      </Box>
-    </Container>
+    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vh'}}>
+      <Container sx={{bgcolor:'#262626', borderRadius: 5, margin: 2, padding: 2}}>
+          <Box sx={{marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', bgcolor:'#262626'}}>
+            <Avatar sx={{bgcolor: 'red'}}></Avatar>
+            <Typography variant='h5' sx={{fontFamily: 'Copperplate Gothic Bold, serif'}}>
+              Sign In
+            </Typography>
+            <TextField sx={{
+              marginBottom: 2, 
+              marginTop: 2, 
+              backgroundColor: '#FFFDD0', 
+              borderRadius: '10px', 
+              '& .MuiInputLabel-root.Mui-focused': {color: '#FF1B1C'}
+            }} id='email' label='Email Address' autoComplete='email' autoFocus/>
+              
+            <TextField sx={{
+              marginBottom: 2, 
+              backgroundColor: '#FFFDD0', 
+              borderRadius: '10px', 
+              '& .MuiInputLabel-root.Mui-focused': {color: '#FF1B1C'},
+              '&.Mui-focused fieldset': {borderColor: '#FF1B1C'}
+            }} variant="outlined" id='password' label='Password' type='password'/>
+            <Button sx={{backgroundColor: '#FF1B1C !important', fontFamily: 'Copperplate Gothic Bold, serif'}} variant='contained'>
+              Sign In
+            </Button>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
