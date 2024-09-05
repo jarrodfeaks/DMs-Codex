a08d01a7bfa1cbf53d8f73ea43eb2624f829da6a
MosaabSaleem <mmosaab2002@hotmail.com>
2024-09-05 20:38:10 +1000

    fixed the errors in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
 // App.tsx
 import React from 'react';
-import { createTheme } from '@mui/material';
 import LoginPage from './LoginPage';
-
-const theme = createTheme({
-  palette: {}
-});
 
 const App: React.FC = () => {
   return (
