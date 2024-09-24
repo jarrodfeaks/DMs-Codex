33b4d6dde1d24e341c2386145dba35b5fe32127c
Sydney Melendres <sydneymelendres@gmail.com>
2024-09-24 23:42:27 +1000

    add draft initiative pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 import Encounter_Screen from './Encounter_Screen.tsx'
 import './index.css'
+import InitiativeGreenPlusAIGenerated from './InitiativeGreenPlusAIGenerated.tsx'
+import InitiativeGreenPlusBestiary from './InitiativeGreenPlusBestiary.tsx'
+import InitiativeGreenPlusPlayerList from './InitiativeGreenPlusPlayerList.tsx'
+import InitiativeRedMinus from './InitiativeRedMinus.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    <Encounter_Screen />
+    {/* <Encounter_Screen /> */}
+    {/* <InitiativeGreenPlusAIGenerated/> */}
+    {/* <InitiativeGreenPlusBestiary /> */}
+    {/* {<InitiativeGreenPlusPlayerList/>} */}
+    {/* <InitiativeRedMinus /> */}
   </StrictMode>,
 )
