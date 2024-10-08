69fe300369d5e409ab29a37fd3561652c66240e9
Justin Tran <jhtran023@gmail.com>
2024-10-08 15:44:17 +1100

    Utils: Add dice rolls, calculation for attack and combat log strings

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,3 +1,13 @@
+/**
+    Calculates a random roll for a X-sided dice
+    @param sides - The number of sides of the dice.
+    @returns True if the character got hit, false otherwise.
+
+    ! Sides will change from number to enum to have a set amount of dice types they can roll.
+*/
+export function rollDice(sides: number): number {
+    return Math.floor(Math.random() * sides) + 1;
+}
 
 /**
     Rolls a 20-sided die or adds rolled values and adds the given modifiers.
@@ -12,9 +22,34 @@ export function rollsWithModifiers(proficiencyBonus: number, abilityModifier: nu
 }
 
 /**
+    Calculates if character got hit.
+    @param roll - The character roll.
+    @param abilityModifier - The ability modifier of the attack. (Varies from each weapon)
+    @param proficiencyBonus - The proficiency bonus of the character.
+    @param armorClass - The armor class of the target.
+    @returns True if the character got hit, false otherwise.
+*/
+export function calculateCharacterHit(roll: number, attackModifier: number, armorClass: number, proficiencyBonus?: number,): boolean {
+    if (proficiencyBonus === null || proficiencyBonus === undefined) {
+        proficiencyBonus = 0;
+    }
+    return roll + attackModifier + proficiencyBonus >= armorClass;
+}
+
+/**
+    Calculate character damage.
+    @param rolls - The character roll.
+    @param weaponModifier - The attack modifier of the attacker.
+    @returns The damage dealt.
+*/
+export function calculateCharacterDamage(rolls: number, weaponModifier: number): number {
+    return rolls + weaponModifier;
+}
+
+/**
     Calculates the proficiency bonus based on character level.
-    @param {number} characterLevel - The level of the character.
-    @returns {number} The proficiency bonus for the given level.
+    @param characterLevel - The level of the character.
+    @returns The proficiency bonus for the given level.
 */
 export function calculateProficiencyBonus(characterLevel: number): number {
     if (characterLevel >= 1 && characterLevel <= 4) {
@@ -30,4 +65,36 @@ export function calculateProficiencyBonus(characterLevel: number): number {
     } else {
         throw new Error('Character level must be between 1 and 20');
     }
+}
+
+/**
+    Sort Queue List by initiative Rolls
+    @param queue - The queue list to sort.
+    @returns // Array of the list sorted by initiative rolls.
+*/
+export function sortQueueByInitiative(queue: Array<{ name: string, initiative: number }>): Array<{ name: string, initiative: number }> {
+    return queue.sort((a, b) => b.initiative - a.initiative);
+}
+
+/**
+    Returns hit attack string for the combat log.
+    @param characterName - The name of the character.
+    @param weapon - The action the character is taking.
+    @param target - The target of the action.
+    @param damage - The damage dealt.
+    @returns The default combat log string.
+*/
+export function attackCombatLogString(characterName: string, weapon: string, target: string, damage: number): string {
+    return `${characterName} SUCCESSFULLY attacked ${target} with a ${weapon} and dealt ${damage}!`;
+}
+
+/**
+    Returns missed attack string for the combat log.
+    @param characterName - The name of the character.
+    @param weapon - The action the character is taking.
+    @param target - The target of the action.
+    @returns The default combat log string.
+*/
+export function missedCombatLogString(characterName: string, weapon: string, target: string): string {
+    return `${characterName} FAILED to attack ${target} with a ${weapon}!`;
 }
