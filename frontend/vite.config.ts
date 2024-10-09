c253e4e123dffe760d8ce81d8fe6d02011924e85
Sydney Melendres <sydneymelendres@gmail.com>
2024-10-09 12:08:42 +1100

    add to .ignore for sydneys port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: `http://localhost:${process.env.PORT || 5000}`,
+        target: `http://localhost:${process.env.PORT || 8080}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
