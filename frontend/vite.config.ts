0abf4652127f9a81d99965198dac26de4476419b
jarrodfeaks <85427306+jarrodfeaks@users.noreply.github.com>
2024-10-17 14:12:22 +1100

    fix environment variables not being loaded in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: Number(process.env.FRONTEND_PORT) || 5173,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: `http://localhost:${process.env.PORT || 5000}`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve(process.cwd(), '..'), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.FRONTEND_PORT) || 5173,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: `http://localhost:${env.PORT || 5000}`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
-    },
+    }
   }
 })
